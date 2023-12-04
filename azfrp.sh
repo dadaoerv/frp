7c987a48dd0d15058f5e4f9de88a6bbc6957f4ca
dadaoerv <46274596+dadaoerv@users.noreply.github.com>
2023-12-04 10:52:00 +0800

    Update azfrp.sh

--- azfrp.sh
+++ azfrp.sh
@@ -20,14 +20,15 @@ rm -rf frp_${FRP_VERSION}_linux_amd64.tar.gz frp_${FRP_VERSION}_linux_amd64
 echo "Configuring frp..."
 sudo tee $FRP_INSTALL_DIR/frpc.ini > /dev/null << EOL
 [common]
-server_addr = your_server_ip
+server_addr = 101.42.151.229
 server_port = 7000
+token = aaa999999
 
 [ssh]
 type = tcp
 local_ip = 127.0.0.1
 local_port = 22
-remote_port = 6000
+remote_port = 2212
 EOL
 
 # 启动frp服务
@@ -35,3 +36,27 @@ echo "Starting frp..."
 sudo $FRP_INSTALL_DIR/frpc -c $FRP_INSTALL_DIR/frpc.ini &
 
 echo "frp installation completed!"
+
+# 创建frp服务文件
+echo "正在创建frp服务文件..."
+cat <<EOF | sudo tee /etc/systemd/system/frp.service
+[Unit]
+Description=frp Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/frpc -c /etc/frp/frpc.ini
+Restart=on-failure
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 启动并设置自动启动
+echo "正在启动frp服务..."
+sudo systemctl daemon-reload
+sudo systemctl start frp.service
+sudo systemctl enable frp.service
+
+echo "frp服务已成功安装并已设置为自动启动。"
